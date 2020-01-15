f6520881f93dfc722d912cd59b77398e6d6096dc
San Neupane <123here.comes.san@gmail.com>
2020-01-15 11:36:10 +0545

    improving bubble sort

--- sorting/bubble-sort.py
+++ sorting/bubble-sort.py
@@ -1,13 +1,20 @@
 if __name__ == "__main__":
     n = int(input("How many numbers ?\t"))
-    nums = [int(x) for x in input("Enter {} numbers\t".format(n)).split()]
+    nums = [int(x) for x in input("Enter {} numbers\t".format(n)).split()]    
 
     for i in range(n-1):       
+
+        #set changed to false, false means no swapping of elements occured
+        changed = False
+
         for j in range(n-i-1):
             if(nums[j] > nums[j+1]):
-                temp = nums[j]
-                nums[j] = nums[j+1]
-                nums[j+1] = temp
+                nums[j],nums[j+1] = nums[j+1],nums[j]              
+                changed = True             #some element is swapped
+
+        #if no element is swapped then no need for further looping , break outer loop
+        if not changed :             
+            break
 
     for i in nums:
         print(i,end=' ')
