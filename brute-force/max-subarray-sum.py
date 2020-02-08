9ec1933f73ef410027f7acba450b1161bc53a1e1
San Neupane <123here.comes.san@gmail.com>
2020-02-08 13:06:12 +0545

    minor changes

--- brute-force/max-subarray-sum.py
+++ brute-force/max-subarray-sum.py
@@ -1,29 +1,39 @@
 # finding the max subarray sum using bruteforce method:
 
+import sys  # to get the minimum value
+
+
 def maxsubarraysum(arr):
-    m = -10000
-    for i in range(0, len(arr)):             # i determines the start
-        for j in range(i, len(arr)):         # j determines the end
+    # initially set m to min possible value
+    m = -sys.maxsize - 1
+
+    # i determines the start
+    for i in range(0, len(arr)):
+        # j determines the end
+        for j in range(i, len(arr)):
             # set sum to 0 and find sum of array[i] to array[j]
             sum = 0
-            for k in range(j, len(arr)-i):   # k loops from i to j and finds sum in that section
+            # k loops from i to j and finds sum in that section
+            for k in range(i,j+1):                
                 sum += arr[k]
-                m = max(m, sum)
+            # sum in that section is saved to m only if it is max than previous sum
+            m = max(m, sum)
 
     return m
+
 
 # minor improvement on the brute-force algorithm
 # instead of looping from i to j and finding sum, we may add a single new element to previous sum
 
-
 def maxsubarraysum2(arr):
-    m = -10000
+    m = -sys.maxsize - 1
     for i in range(0, len(arr)):
         sum = 0
         for j in range(i, len(arr)):
             sum += arr[j]
             m = max(m, sum)
     return m
+
 
 if __name__ == "__main__":
 
--- dp/max-subarray-sum.py
+++ dp/max-subarray-sum.py
@@ -4,13 +4,26 @@ if __name__ == "__main__":
     arr = [10, 5, -12, 7, -10, 20, 30, -10, 50, 60]
     size = len(arr)
 
+    # To calculate the sum while iterating the array,
+    # may reset value if sum is negative
     max_end_here = 0
+
+    # To store the largest possible ans at any instance
+    # don't set to 0, as all negative element may also be present where ans < 0
     max_so_far = arr[0]
 
     for i in range(0, len(arr)):
+        # reset if negative
         if max_end_here < 0:
             max_end_here = 0
+
+        # add the array element
+        # since we reset earlier,
+        # so if current element is negative it won't be replaced by 0 &
+        # it may be counted as ans if it is max
         max_end_here += arr[i]
+
+        # if it is max at current instance, store it in max_so_far
         if max_so_far < max_end_here:
             max_so_far = max_end_here
 
