ae9af49a3c9a01a88d4b950c9d73850dbc78951f
Nepali-San <sanneupane1024@gmail.com>
2020-04-06 13:06:35 +0545

    some string algo added

---
+++ strings/KMP.py
@@ -0,0 +1,76 @@
+# compoutes Longest Prefix Suffix array of pattern
+def computeLPS(pat):
+
+    n = len(pat)
+
+    # create lps[] that will hold the longest prefix suffix
+    # values for pattern
+    lps = [0] * n
+
+    # first point to index 1 of pattern and second points to index 2 of pattern
+    # first and second indexes are used to compare whether the string are equal or not
+    first = 1
+    second = 0
+
+    while first < n:
+
+        if pat[first] == pat[second]:
+            # move second forward by 1
+            second += 1
+            # set lps[first] to second, i.e. upto index first we have same prefix/suffix
+            lps[first] = second
+            # move first forward by 1
+            first += 1
+        else:
+            # if unmatched and second is at it's initial position
+            if second == 0:
+                # set 0 i.e. no common prefix/suffix
+                lps[first] = 0
+                # move first by 1
+                first += 1
+            else:
+                # at this point we might have some common suffix and prefix
+                # move second back to the index lps[second-1]
+                # i.e. ignore the prefix which is also a suffix and continue from ahead of the prefix
+                # this part uses the degeneration property of pattern
+                second = lps[second - 1]
+                # note: first is not incremented, we compare the new second index to the current
+                # first index to find if a match is possible
+
+    return lps
+
+
+def kmp(text, pat):
+    # Preprocess the pattern (calculate lps[] array)
+    lps = computeLPS(pat)
+
+    N = len(text)
+    n = len(pat)
+
+    second = 0  # index for text
+    first = 0  # index of pat
+
+    while first < N:
+
+        if pat[second] == text[first]:
+            first += 1
+            second += 1
+
+        # if every string of pattern is compared then patter in found.
+        if second == n:
+            print(f"Pattern found at {first-second}")
+            second = lps[second - 1]
+        # if pattern stops matching
+        elif first < N and pat[second] != text[first]:
+            # if second != 0, set second to lps[second-1] i.e. don't compare the common prefix
+            if second != 0:
+                second = lps[second - 1]
+            else:
+                first += 1
+
+
+if __name__ == "__main__":
+    text = "ABABDABACDABABCABAB"
+    pat = "ABABCABAB"
+
+    kmp(text, pat)
---
+++ strings/naive_string_search.py
@@ -0,0 +1,23 @@
+if __name__ == "__main__":
+    text = input("Enter the text:\t")
+    search = input("Enter the word to search in the text:\t")
+
+    n = len(text)
+    m = len(search)
+
+    for i in range(n - m + 1):
+
+        # Compare ith character of text string with first character of search string
+        # if they doesn't match continue with the next character of text string
+        if text[i] != search[0]:
+            continue
+
+        # if matched, check other characters for the match until there is no match or all character are compared
+        j = 1
+        while (j < m) and (text[i + j] == search[j]):
+            j += 1
+
+        # if the while loop ends with all character of search string compared, then the match is found
+        if j == m:
+            print(f"Found at index {i+1}")
+
